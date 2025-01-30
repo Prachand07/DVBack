b28fb26c7f8446e59179bdb1cc6372340d386cca
Prachand07 <justluthra910@gmail.com>
2025-01-31 02:28:54 +0530

    Changes in port, set to 8090

--- public/configure.sh
+++ public/configure.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd
-sudo yum install -y nodejs
+
 sudo systemctl start httpd
 sudo systemctl enable httpd
 cd /var/www/html/backend
+sudo yum install -y nodejs
 node server.js
 echo "Deployment complete and server restarted successfully."
