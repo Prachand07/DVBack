0d28d51cd147849146e2804a2cd9ecf002110f2a
Aarush Luthra <163556661+Prachand07@users.noreply.github.com>
2025-01-31 02:01:56 +0530

    Update configure.sh

--- public/configure.sh
+++ public/configure.sh
@@ -3,5 +3,6 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
+cd /var/www/html/backend
 node server.js
 echo "Deployment complete and server restarted successfully."
