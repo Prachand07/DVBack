4f13b507e23267eefdfac2b6c26b72b468da0377
Prachand07 <justluthra910@gmail.com>
2025-01-31 22:34:46 +0530

    Changed backend for public ip, modified bash file

--- public/configure.sh
+++ public/configure.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo yum update -y
 sudo yum install -y httpd
-
 sudo systemctl start httpd
 sudo systemctl enable httpd
 cd /var/www/html/backend
 sudo yum install -y nodejs
+sudo fuser -k 8090/tcp
 node server.js
 echo "Deployment complete and server restarted successfully."
