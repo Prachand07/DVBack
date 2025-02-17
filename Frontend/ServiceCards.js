5cd4e8280134e1f4a0e19c70256aaaa69db76b81
Prachand07 <justluthra910@gmail.com>
2025-02-17 21:05:02 +0530

    Image path modified

--- Frontend/ServiceCards.js
+++ Frontend/ServiceCards.js
@@ -1,36 +1,36 @@
 const cardData = [
     {
         title: "Static Hosting Pro",
-        image: "../images/SimpleStorageService-bgr.png",
+        image: "Images/SimpleStorageService-bgr.png",
         description: "Fast, scalable hosting for your static sites.",
         features: [
-            { icon: "../images/stocks.png",  text: "Reliable hosting with minimal downtime" },
-            { icon: "../images/lightning.png", text: "Lightning-fast page loads, ensuring seamless content delivery" },
-            { icon: "../images/shield.png", text: "Robust Security with SSL and DDoS protection." },
-            { icon: "../images/scaling.png",style: { width: "50px", height: "50px" },  text: "Easy Scaling with support for parallel requests." }
+            { icon: "Images/stocks.png",  text: "Reliable hosting with minimal downtime" },
+            { icon: "Images/lightning.png", text: "Lightning-fast page loads, ensuring seamless content delivery" },
+            { icon: "Images/shield.png", text: "Robust Security with SSL and DDoS protection." },
+            { icon: "Images/scaling.png",style: { width: "50px", height: "50px" },  text: "Easy Scaling with support for parallel requests." }
         ]
     },
 
     {
         title: "EC2 DualHost",
-        image: "../images/ec2-nbgr.png",
+        image: "Images/ec2-nbgr.png",
         description: "Powering both frontend & backend on EC2.",
         features: [
-            { icon: "../images/laptop.png",  text: "Run your Frontend and Backend code at one place" },
-            { icon: "../images/lightning.png", text: "Seamless, high-speed, and dependable execution on EC2 " },
-            { icon: "../images/globe.png", text: "Get a public IP to access your hosted application" },
-            { icon: "../images/tools.png", text: "No manual configurations—just upload and go!" }
+            { icon: "Iimages/laptop.png",  text: "Run your Frontend and Backend code at one place" },
+            { icon: "Images/lightning.png", text: "Seamless, high-speed, and dependable execution on EC2 " },
+            { icon: "Images/globe.png", text: "Get a public IP to access your hosted application" },
+            { icon: "Images/tools.png", text: "No manual configurations—just upload and go!" }
         ]
     },
     {
         title: "All-in-One Deploy",
-        image: "../images/rdec-nbr.png",
+        image: "Images/rdec-nbr.png",
         description: "Seamless hosting for logic and data layers.",
         features: [
-            { icon: "../images/shield.png", text: "Built-in security measures for all layers" },
-            { icon: "../images/link.png", text: " Host your application logic on EC2 with MySQL RDS support" },
-            { icon: "../images/tools.png", text: "Preconfigured environments for a hassle-free exprience" },
-            { icon: "../images/backup.png", text: " MySQL comes with automated backups and point-in-time recovery" }
+            { icon: "Images/shield.png", text: "Built-in security measures for all layers" },
+            { icon: "Images/link.png", text: " Host your application logic on EC2 with MySQL RDS support" },
+            { icon: "Images/tools.png", text: "Preconfigured environments for a hassle-free exprience" },
+            { icon: "Images/backup.png", text: " MySQL comes with automated backups and point-in-time recovery" }
         ]
     }
 ];
