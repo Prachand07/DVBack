a32689e21fd534268a60899c305eee781dc10fb1
Aditya-04030 <aditya123sahniaz@gmail.com>
2025-04-10 14:14:38 +0530

    validation finalized

--- configure.sh
+++ configure.sh
@@ -14,11 +14,11 @@ gpgkey=https://pgp.mongodb.com/server-8.0.asc" | sudo tee $REPO_FILE > /dev/null
 sudo yum install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
-aws ssm get-parameters --names "JWT_SECRET" "MONGO_URL" --with-decryption --query "Parameters[*].{Name:Name,Value:Value}" --output text | while read name value; do
+sudo aws ssm get-parameters --names "JWT_SECRET" "MONGO_URL" --with-decryption --query "Parameters[*].{Name:Name,Value:Value}" --output text | while read name value; do
   export "$name"="$value"
 done
 
-aws ssm get-parameter --name "KP" --with-decryption --query "Parameter.Value" --output text >/var/www/html/Backend/Ec2-DV.pem    
+sudo aws ssm get-parameter --name "KP" --with-decryption --query "Parameter.Value" --output text >/var/www/html/Backend/Ec2-DV.pem    
 sudo chmod 400 /var/www/html/Backend/Ec2-DV.pem          
 sudo echo "CONFIG = { PUBLIC_IP: '$(curl -s ifconfig.me)' };" > config.js
 sudo yum install -y nodejs
